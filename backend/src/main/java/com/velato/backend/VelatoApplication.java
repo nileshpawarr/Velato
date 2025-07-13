d8ecb3370d92e60b215b30fd4bd9784ed90523b9
Nilesh <nileshpawar9922@gmail.com>
2025-07-13 18:13:13 +0200

    BE initial command

---
+++ backend/src/main/java/com/velato/backend/VelatoApplication.java
@@ -0,0 +1,13 @@
+package com.velato.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VelatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VelatoApplication.class, args);
+	}
+
+}
